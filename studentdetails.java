131bae9f42ac56f37838f6799cb479a1b8622201
KarnatiNarmada <105156199+KarnatiNarmada@users.noreply.github.com>
2022-06-24 21:56:59 +0530

    searchrecordPackage

---
+++ studentdetails.java
@@ -0,0 +1,5 @@
+package searchrecord;
+public class studentdetails{
+	public String first_name,last_name,middle_name,roll_no,branch,section,contact_no,father_name,pin,father_ph,mandal,district,locality,state,house_no;
+	public int number=0;
+}

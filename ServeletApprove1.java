0ef74e78fea55bdc5d7b7f32d93d0e45d58eb102
KarnatiNarmada <105156199+KarnatiNarmada@users.noreply.github.com>
2022-06-24 22:00:53 +0530

    EBlock-package

---
+++ ServeletApprove1.java
@@ -0,0 +1,43 @@
+package EBlock;
+import java.sql.*;
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import request.connections;
+@WebServlet("/ServletApprove1")
+public class ServeletApprove1 extends HttpServlet 
+{
+    private static final long serialVersionUID = 1L;
+    public ServeletApprove1() 
+    {
+        super();
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        PrintWriter out=response.getWriter();
+        //jdbc code for connectivity with oracle database.
+        try
+        {  
+        	Connection con=connections.Establish();
+            Statement stmt=con.createStatement();  
+            String id = request.getParameter("ID");
+            stmt.executeUpdate("UPDATE student_request SET EBlock_status='approved' WHERE roll_no='"+id+"'");
+            out.println("<script type=\"text/javascript\">");
+            out.println("alert('your submission has been successfully approved');");
+            out.println("location='Eblockpage.jsp';");
+            out.println("</script>");      
+        }
+        catch (Exception e2) 
+        {
+
+        }
+
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+
+    }
+}
